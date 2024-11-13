881bc18db77cc135eca424b421ca2fd08aadd5bd
sabitsadiq <sabitsdq@gmail.com>
2024-11-13 16:31:53 +0100

    updated the crud operation

--- index.ts
+++ index.ts
@@ -4,15 +4,30 @@ const prisma = new PrismaClient();
 
 async function main() {
   // create
-  const user = await prisma.user.create({
-    data: {
-      name: "john doe",
-      email: "john@gmail.com",
-    },
-  });
-  console.log(user);
-}
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "john doe",
+  //       email: "john@gmail.com",
+  //     },
+  //   });
+  //   const users = await prisma.user.findMany();
 
+  //   create article and associate it with a user
+  //   const article = await prisma.article.create({
+  //     data: {
+  //       title: "Johns first article",
+  //       body: "This is john first article",
+  //       author: {
+  //         connect: {
+  //           id: 1,
+  //         },
+  //       },
+  //     },
+  //   });
+  // get all the articles
+  const articles = await prisma.article.findMany();
+  console.log(articles);
+}
 main()
   .then(async () => {
     await prisma.$disconnect();
