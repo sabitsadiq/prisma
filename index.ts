5144b0f5c716bc91aa29bdcf54e2a968a9186315
sabitsadiq <sabitsdq@gmail.com>
2024-11-13 16:20:43 +0100

    connected to db and created user

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // create
+  const user = await prisma.user.create({
+    data: {
+      name: "john doe",
+      email: "john@gmail.com",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
